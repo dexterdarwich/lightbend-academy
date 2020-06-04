cefbdd7c2240c593adf69ef65342da971379e5d7
Dexter Darwich <dex.darwich@gmail.com>
2020-06-03 21:39:50 -0600

    Use method reference

--- Lightbend-Java-Language-Lambdas/exercises/src/main/java/com/lightbend/lambdas/MessageTransformers.java
+++ Lightbend-Java-Language-Lambdas/exercises/src/main/java/com/lightbend/lambdas/MessageTransformers.java
@@ -6,16 +6,16 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 class MessageTransformers {
-    static Function<Message, String> toString = message -> message.toString();
+    static Function<Message, String> toString = Message::toString;
 
-    static Function<Message, Instant> toTimestamp = message -> message.getTimestamp();
+    static Function<Message, Instant> toTimestamp = Message::getTimestamp;
 
     static Function<Message, Message> toUpperCase = message -> new Message(message.getId(), message.getTimestamp(), message.getContent().toUpperCase());
 
     static <T> List<T> mapMessages(List<Message> messages, Function<Message, T> transformer) {
         return messages
                 .stream()
-                .map(message -> transformer.apply(message))
+                .map(transformer)
                 .collect(Collectors.toList());
     }
 }
