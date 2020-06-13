4b582087d8b6273198c4219e96fc1d34294d10a3
Dexter Darwich <dex.darwich@gmail.com>
2020-06-12 23:07:09 -0600

    Fault Tolerance

--- LTJ-P-lightbend-akka-streams-for-java-professional-v1/exercises/exercises/src/main/java/com/lightbend/akkassembly/QualityAssurance.java
+++ LTJ-P-lightbend-akka-streams-for-java-professional-v1/exercises/exercises/src/main/java/com/lightbend/akkassembly/QualityAssurance.java
@@ -1,19 +1,42 @@
 package com.lightbend.akkassembly;
 
 import akka.NotUsed;
+import akka.stream.ActorAttributes;
+import akka.stream.Supervision;
 import akka.stream.javadsl.Flow;
+import akka.japi.function.Function;
 
 public class QualityAssurance {
+    static class CarFailedInspection extends IllegalStateException {
+        public CarFailedInspection(UnfinishedCar car) {
+            super("Car Failed Inspection: " + car);
+        }
+    }
+
     private final Flow<UnfinishedCar, Car, NotUsed> inspect;
 
     public QualityAssurance() {
-        inspect = Flow.of(UnfinishedCar.class).filter(this::isValidCar).map(car ->
-                new Car(new SerialNumber(),
-                        car.getColor().get(),
-                        car.getEngine().get(),
-                        car.getWheels(),
-                        car.getUpgrade())
-        );
+        Function<Throwable, Supervision.Directive> decider = ex -> {
+            if(ex instanceof CarFailedInspection) {
+                return Supervision.resume();
+            } else {
+                return Supervision.stop();
+            }
+        };
+
+        inspect = Flow.of(UnfinishedCar.class)
+                .map(car -> {
+                    if(isValidCar(car))
+                        return new Car(
+                                new SerialNumber(),
+                                car.getColor().get(),
+                                car.getEngine().get(),
+                                car.getWheels(),
+                                car.getUpgrade()
+                        );
+                    else
+                        throw new CarFailedInspection(car);
+                }).withAttributes(ActorAttributes.withSupervisionStrategy(decider));
     }
 
     public Flow<UnfinishedCar, Car, NotUsed> getInspect() {
