b253210435f78d3fb82c5fec8403b26407855605
Dexter Darwich <dex.darwich@gmail.com>
2020-06-13 23:09:05 -0600

    Upgrade Shop (Graphs & Shapes)

--- LTJ-P-lightbend-akka-streams-for-java-professional-v1/exercises/exercises/src/main/java/com/lightbend/akkassembly/Factory.java
+++ LTJ-P-lightbend-akka-streams-for-java-professional-v1/exercises/exercises/src/main/java/com/lightbend/akkassembly/Factory.java
@@ -13,14 +13,16 @@ public class Factory {
     private final EngineShop engineShop;
     private final WheelShop wheelShop;
     private final QualityAssurance qualityAssurance;
+    private final UpgradeShop upgradeShop;
     private final Materializer materializer;
 
-    public Factory(BodyShop bodyShop, PaintShop paintShop, EngineShop engineShop, WheelShop wheelShop, QualityAssurance qualityAssurance, Materializer materializer) {
+    public Factory(BodyShop bodyShop, PaintShop paintShop, EngineShop engineShop, WheelShop wheelShop, QualityAssurance qualityAssurance, UpgradeShop upgradeShop, Materializer materializer) {
         this.bodyShop = bodyShop;
         this.paintShop = paintShop;
         this.engineShop = engineShop;
         this.wheelShop = wheelShop;
         this.qualityAssurance = qualityAssurance;
+        this.upgradeShop = upgradeShop;
         this.materializer = materializer;
     }
 
@@ -29,6 +31,7 @@ public class Factory {
                 .via(paintShop.getPaint())
                 .via(engineShop.getInstallEngine())
                 .via(wheelShop.getInstallWheels())
+                .via(upgradeShop.getInstallUpgrades())
                 .via(qualityAssurance.getInspect())
                 .take(quantity)
                 .toMat(Sink.seq(), Keep.right())
